c75b42459f6f69afcc68460118250ec16dac02bd
Morning <springoflotus@gmail.com>
2025-10-24 14:35:42 +0800

    feat:完成ChatModel和ChatTemplate

--- AwesomeEino/main.go
+++ AwesomeEino/main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 
 	"github.com/cloudwego/eino-ext/components/model/ark"
+	"github.com/cloudwego/eino/components/prompt"
 	"github.com/cloudwego/eino/schema"
 	"github.com/joho/godotenv"
 )
@@ -20,28 +21,50 @@ func main() {
 		Model:  os.Getenv("ARK_CHAT_MODEL"),
 	})
 
-	input := []*schema.Message{
-		schema.SystemMessage("你是一个爱唠叨的老妈"),
-		schema.UserMessage("提醒我每天早睡早起"),
+	template := prompt.FromMessages(schema.FString,
+		schema.SystemMessage("你是一个{role}"),
+		&schema.Message{
+			Role:    schema.User,
+			Content: "提醒我每天{task}",
+		},
+	)
+
+	params := map[string]any{
+		"role": "爱睡觉的老爸",
+		"task": "好好学习，天天向上",
 	}
+
+	// input := []*schema.Message{
+	// 	schema.SystemMessage("你是一个爱唠叨的老妈"),
+	// 	schema.UserMessage("提醒我每天早睡早起"),
+	// }
+
+	message, err := template.Format(ctx, params)
+
+	response, err := model.Generate(ctx, message)
+	if err != nil {
+		panic(err)
+	}
+	print(response.Content)
+
 	// respose, err := model.Generate(ctx, input)
 	// if err != nil {
 	// 	panic(err)
 	// }
 	// print(respose.Content)
 
-	reader, err := model.Stream(ctx, input)
-	if err != nil {
-		panic(err)
-	}
-	defer reader.Close()
+	// reader, err := model.Stream(ctx, input)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// defer reader.Close()
 
-	for {
-		chunk, err := reader.Recv()
-		if err != nil {
-			panic(err)
-		}
+	// for {
+	// 	chunk, err := reader.Recv()
+	// 	if err != nil {
+	// 		panic(err)
+	// 	}
 
-		print(chunk.Content)
-	}
+	// 	print(chunk.Content)
+	// }
 }
